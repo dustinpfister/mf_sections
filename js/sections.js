64154e9d2afc31e7db90ac780acb378b1f8d1e82
dustinpfister <dustin.pfister@gmail.com>
2017-08-20 08:08:58 -0400

    sections positions looking good

--- js/sections.js
+++ js/sections.js
@@ -1,52 +1,42 @@
 var S = (function () {
 
-    // the view port
-    var vp = {
-
-        w : 320,
-        h : 240,
-
-        // set up
-        set : function () {
-
-            this.x = -this.w / 2;
-            this.y = -this.h / 2;
-
-        }
-
-    },
-
     // the section map
-    map = {
+    var map = {
 
-        sw : 640, // the pixel width and height of a section
-        sh : 480,
-        W : 4, // the section matrix width and height
-        H : 4,
+        sw : 100, // the pixel width and height of a section
+        sh : 100,
+        W : 2, // the section matrix width and height
+        H : 2,
 
         secs : [], // the sections array
+        load : [], // currently loaded sections
 
         // setup sections
         set : function () {
 
             var X,
-            Y = 0,
+            Y = -this.H / 2,
             x,
             y;
 
             this.secs = [];
-            while (Y < this.H) {
+            while (Y < this.H / 2) {
 
-                X = 0;
-                while (X < this.W) {
+                X = -this.W / 2;
+                while (X < this.W / 2) {
 
-                    x = this.W / 2 * -this.sw + this.sw * X;
-                    y = this.W / 2 * -this.sw + this.sw * X;
+                    //x = this.W / 2 * this.sw + this.sw * X;
+                    //y = this.H / 2 * this.sh + this.sh * Y;
+
+                    x = X * this.sw;
+                    y = Y * this.sh;
 
                     // push new section object
                     this.secs.push({
 
-                        x : x,
+                        X : X, // cell pos
+                        Y : Y,
+                        x : x, // px pos
                         y : y
 
                     });
@@ -60,8 +50,32 @@ var S = (function () {
 
         }
 
-    };
+    },
 
+    // the view port
+    vp = {
+
+        w : 320,
+        h : 240,
+
+        // set up
+        set : function () {
+
+            this.x = -this.w / 2;
+            this.y = -this.h / 2;
+
+        },
+
+        // load sections based on current view port position
+        ls : function () {
+
+            this.load = [];
+
+        }
+
+    },
+
+    // the public api
     api = {
 
         // ref to map
