dcb75dccc56a39bbdaa8646c1554b8c6c8e7b621
dustinpfister <dustin.pfister@gmail.com>
2017-08-25 09:23:29 -0400

    basic demo update

--- demos/basic.js
+++ demos/basic.js
@@ -1,18 +1,34 @@
 
+// map section size (pixels)
 S.map.sw = 800;
 S.map.sh = 600;
+
+// map section size (section count)
 S.map.W = 4;
 S.map.H = 4;
 
+// map view port size (pixels)
 S.vp.w = 320;
 S.vp.h = 240;
 
+// setup map, and view port
 S.map.set();
 S.vp.set();
 
+// load current section
 S.vp.ls();
 
+console.log('section count: ' + S.map.secs.length); // 16 sections
+console.log('loaded count: ' + S.map.load.length); // 4 sections loaded
 
+// change view port position
+S.vp.x = -1600;
+S.vp.y = -1200;
+
+S.vp.ls();
+console.log('loaded count: ' + S.map.load.length); // 1 sections loaded
+
+// objects
 console.log('S object');
 console.log(S);
 
@@ -21,7 +37,6 @@ console.log(S.vp);
 
 console.log('Sections Array: ');
 console.log(S.map.secs);
-
 
 console.log('Loaded Sections Array: ');
 console.log(S.map.load);
