526baa7e4b43f83d426ae243a369a55ba904f27f
dustinpfister <dustin.pfister@gmail.com>
2017-08-31 10:51:24 -0400

    mf_sections.js

---
+++ mf_sections.js
@@ -0,0 +1,143 @@
+var S = {
+
+    sw : 320, // the pixel width and height of a section
+    sh : 240,
+    W : 8, // the section matrix width and height
+    H : 5,
+    la : 1,
+
+    secs : [], // the sections array
+    load : [], // currently loaded sections
+
+    // get a section by x, and y pixel location
+    getPos : function (x, y, debug) {
+
+        return this.get(Math.floor(x / this.sw), Math.floor(y / this.sh), debug);
+
+    },
+
+    // get a section by X & Y sec pos
+    get : function (X, Y, debug) {
+
+        var hw = Math.ceil(this.W / 2),
+        hh = Math.ceil(this.H / 2);
+
+        if (X >= -hw && X < hw && Y >= -hh && Y < hh) {
+
+            var i = this.W * (Y + hh) + X + hw,
+
+            sec = this.secs[i];
+
+            return sec
+
+        };
+
+        return {};
+
+    },
+
+    // setup sections
+    set : function (sw, sh, W, H) {
+
+        var X,
+        Y,
+        x,
+        y;
+
+        this.sw = sw || 320;
+        this.sh = sh || 240;
+        this.W = W || 8;
+        this.H = H || 5;
+
+        this.secs = [];
+        Y = Math.floor(-this.H / 2);
+        while (Y < this.H / 2) {
+
+            X = Math.floor(-this.W / 2);
+            while (X < this.W / 2) {
+
+                x = X * this.sw;
+                y = Y * this.sh;
+
+                // push new section object
+                this.secs.push({
+
+                    i : (Y + Math.floor(this.H / 2)) * this.W + X + Math.floor(this.W / 2),
+                    X : X, // cell pos
+                    Y : Y,
+                    x : x, // px pos
+                    y : y
+
+                });
+
+                X += 1;
+
+            }
+
+            Y += 1;
+        }
+
+    },
+
+    // load sections based on the given vp values
+    ls : function (x, y, w, h) {
+
+        var SX,
+        EX,
+        SY,
+        EY,
+        Y,
+        X,
+        i;
+
+        this.load = [];
+
+        SX = Math.round(x / this.sw);
+        SY = Math.round(y / this.sh);
+        EX = Math.round((x + w) / this.sw);
+        EY = Math.round((y + h) / this.sh);
+
+        Y = SY - this.la;
+        while (Y < EY + this.la) {
+
+            X = SX - this.la;
+            while (X < EX + this.la) {
+
+                var sec = this.get(X, Y);
+
+                if (sec.X != undefined) {
+
+                    this.load.push(sec);
+                }
+
+                X += 1;
+
+            }
+
+            Y += 1;
+
+        }
+
+    }
+
+};
+
+S.set();
+
+/*
+ajust : function (obj, width, height) {
+
+width = width || 640;
+height = height || 480;
+
+return {
+
+x : (obj.x - vp.x) / map.sw * width / vp.mw,
+y : (obj.y - vp.y) / map.sh * height / vp.mh,
+w : obj.s === undefined ? width / vp.mw : obj.s * (11 - S.vp.mh),
+h : obj.s === undefined ? height / vp.mh : obj.s * (11 - S.vp.mh)
+
+};
+
+}
+*/
